f87740457d354f885a6308e3511c5c63de8cecd8
Tim Stevens <timstevens@hush.com>
2018-06-22 00:04:28 -0500

    pushing the fix for submit

--- controllers/userhome-routes.js
+++ controllers/userhome-routes.js
@@ -3,171 +3,153 @@ const path = require("path");
 const fs = require("fs");
 var db = require("../models");
 
-var router = function (app) {
+var router = (app) => {
 
-    app.get('/api/userhome', function (request, response) {
+    ////  ROUTES USED ON INDEX.HANDLEBARS - see inside config/passport.js
+
+
+////////  ROUTES USED ON USERHOME.HANDLEBARS  ///////////////////////////
+
+    app.get('/api/userhome', (request, response) => {
         db.Text.findAll({})
-            .then(function (result) {
-                console.log(result);
-                response.json(result);
-            });
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/userhome', function (request, response) {
+    app.get('/userhome', (request, response) => {
         response.render('userhome')
     });
 
     
-    app.get('/api/lol', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    lol: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/lol', (request, response) => {
+        db.Text.findAll({
+            where: { lol: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/wtfam', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    wtfam: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/wtfam', (request, response) => {
+        db.Text.findAll({
+            where: { wtfam: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/ew', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    ew: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/ew', (request, response) => {
+        db.Text.findAll({
+            where: { ew: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/bff', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    bff: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/bff', (request, response) => {
+        db.Text.findAll({
+            where: { bff: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/lol', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    lol: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/lol', (request, response) => {
+        db.Text.findAll({
+            where: { lol: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/nsfw', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    nsfw: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/nsfw', (request, response) => {
+        db.Text.findAll({
+            where: { nsfw: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/fail', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    fail: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/fail', (request, response) => {
+        db.Text.findAll({
+            where: { fail: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/fail', function (request, response) {
-        db.Text.findAll(
-            {
-                where: {
-                    fail: 1
-                }
-            })
-            .then(function (result) {
-                console.log(response);
-                response.json(result);
-            });
+    app.get('/api/fail', (request, response) => {
+        db.Text.findAll({
+            where: { fail: 1 }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
+///////////////////////////////////////////////////////////////////////
 
+////////  ROUTES USED ON COMMENT.HANDLEBARS  ///////////////////////////
 
-    // findById(textId, { include: [db.Comment] })
-
-
-    app.get('/api/textfocusget/:id', function (request, response) {
+    app.get('/api/textfocusget/:id',(request, response) => {
         var textId = request.params.id;
         db.Text.findById(textId, {
             include: [db.Comment]
         })
-            .then(function (result) {
-                console.log(result);
-                response.json(result);
-            });
+        .then( (result) => {
+            response.json(result);
+        });
     });
 
-
-    app.get('/textfocus/:id', function (request, response) {
+    app.get('/textfocus/:id',(request, response) => {
         response.render('comment')
     });
 
-    app.get('/text/:id', function (request, response) {
-        response.render('submit')
+    app.get('/text/comments/:id',(request, response) => {
+        var textId = request.params.id;
+        db.Comment.findAll({
+            where: { textid: textId }
+        })
+        .then( (result) => {
+            response.json(result);
+        })
+    });
+///////////////////////////////////////////////////////////////////////
+
+////////  ROUTES USED ON SUBMIT.HANDLEBARS  ///////////////////////////
+
+    app.get('/submit-txt-img',(request, response) => {
+        response.render('submit');
     });
 
-    app.get('/text/comments/:id', function (request, response) {
-        var textId = request.params.id;
-        db.Comment.findAll(
-            {
-                where: {
-                    textid: textId
-                }
-            })
-            .then(function (result) {
-                response.json(result);
-            })
+    app.get('/api/submit-text-img',(request, response) => {
+        var loggedInUserId = request.user.id;
+        db.Text.findOne({
+            where: { userId: loggedInUserId },
+            order: [ ['createdAt', 'DESC'] ]
+        })
+        .then( (result) => {
+            response.json(result);
+        })
     });
 
-    app.get('/api/text/:id', function (request, response) {
-        var textId = request.params.id;
-        db.Text.findById(textId)
-            .then(function (result) {
-                console.log(result);
-                response.json(result);
-            })
+    app.put('/api/submit-txt-img', (request, response) => {
+        var loggedInUserId = request.user.id;
+        db.Text.update(request.body,
+            { where: { id: request.body.id } 
+        })
+        .then( (dbText) => {
+            response.json(dbText);
+        })
     });
+
 
     app.post('/text/comments/create', (request, response) => {
         var newComment = request.body;
@@ -179,30 +161,15 @@ var router = function (app) {
 
     app.get('/text/comments', (request, response) => {
         db.Comment.findAll({})
-            .then(function (result) {
+            .then( (result) => {
                 console.log(result);
                 response.json(result);
             });
     })
 
-    app.put('/api/text/:id', function (request, response) {
-        var textId = request.params.id;
-        db.Text.update(request.body,
-            {
-                where: {
-                    id: request.params.id
-                }
-            })
-            .then(function (dbText) {
-                response.json(dbText);
-            })
-
-    });
-
-    app.post('/submit', function (request, response) {
+    app.post('/submit', (request, response) => {
         response.render('submit')
     });
-
 
     var multer = require("multer");
     var handleError = (error, response) => {
@@ -213,12 +180,10 @@ var router = function (app) {
     };
     var upload = multer({ dest: "public/assets/uploads/" });
 
-    app.post("/upload", upload.single("file"), (request, response, next) => {
+    app.post("/upload-text-img", upload.single("file"), (request, response, next) => {
         var extension = path.extname(request.file.originalname).toLowerCase();
         var tempFilePath = request.file.path;
         var newFilePath = path.join("public/assets/uploads/", request.file.filename + extension);
-        console.log(JSON.stringify(request.user));
-
 
         if (extension === ".png") {
             fs.rename(tempFilePath, newFilePath, error => {
@@ -227,9 +192,9 @@ var router = function (app) {
                     image: '/assets/uploads/' + request.file.filename + extension,
                     userId: request.user.id
                 })
-                    .then(function (text) {
-                        response.redirect('/text/' + text.id)
-                    });
+                .then( (text) => {
+                    response.redirect('/submit-txt-img')
+                });
             })
         }
     });
